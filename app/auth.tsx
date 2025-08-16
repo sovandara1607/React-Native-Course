9db485a9bcc14529b98d210b69d4569a08587dc2
Sovandara RIth <srith3@paragoniu.edu.kh>
2025-08-16 18:01:04 +0700

    UI changed Again

--- app/auth.tsx
+++ app/auth.tsx
@@ -113,7 +113,7 @@ const styles = StyleSheet.create({
     title:{
         fontSize: 24,
         fontWeight: "bold",
-        marginBottom: 20,
+        marginBottom: 24,
         
     },
     input: {
