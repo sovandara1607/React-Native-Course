aa89af62fddadcb849b4fdcc286175dbfe6fbfc6
Sovandara RIth <srith3@paragoniu.edu.kh>
2025-08-19 13:15:51 +0700

    change padding back to normal

--- app/auth.tsx
+++ app/auth.tsx
@@ -108,7 +108,7 @@ const styles = StyleSheet.create({
     content: {
         flex: 1,
         justifyContent: "center",
-        padding: 15,
+        padding: 16,
     },
     title:{
         fontSize: 24,
